cff4ea85969e449672b2a725cb9f64b9bfceec8f
Xappel <yorick.brouwer@planet.nl>
2016-12-13 12:20:18 +0100

    sysCtrl.c
    Updated buttons status, bits are now binary.

--- Firmware_Embedded_System/Src/lidar/sysCtrl.c
+++ Firmware_Embedded_System/Src/lidar/sysCtrl.c
@@ -14,14 +14,15 @@
 
 #if SYSCTRL == 1
 
-#define LED_SET_ONE 	00000001
-#define LED_SET_TWO 	00000010
-#define LED_SET_THREE 	00000100
+#define LED_SET_ONE 	0b00000001
+#define LED_SET_TWO 	0b00000010
+#define LED_SET_THREE 	0b00000100
 
 void LED_Control(void* argument);
 void Button_Control(void* argument);
 
 extern uint8_t LED_Register_Bits;
+extern uint8_t Button_Register_Bits;
 
 /*
 	Set Pinouts for LEDs with status of pointer argument.
@@ -53,19 +54,31 @@ void Button_Control(void* argument)
 	uint8_t debounce = 0;
 
 	while(1){
-		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6) != GPIO_PIN_SET && (debounce & 00000001) != 0){
-			LED_Register_Bits ^= 00000001;
-			debounce = 0;
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6) != GPIO_PIN_SET && (debounce & 0b00000001) != 0b00000000){
+			LED_Register_Bits ^= 0b00000001;
+			Button_Register_Bits |= 0b00000001;
+			debounce &= 0b11111110;
 		}else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6) != GPIO_PIN_RESET){
-			debounce = 1;
+			debounce |= 0b00000001;
+			Button_Register_Bits &= 0b11111110;
 		}
 		
-		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) != GPIO_PIN_SET){
-			LED_Register_Bits ^= 00000010;
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) != GPIO_PIN_SET && (debounce & 0b00000010) != 0b00000000){
+			LED_Register_Bits ^= 0b00000010;
+			Button_Register_Bits |= 0b00000010;
+			debounce &= 0b11111101;
+		}else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) != GPIO_PIN_RESET){
+			debounce |= 0b00000010;
+			Button_Register_Bits &= 0b11111101;
 		}
 		
-		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) != GPIO_PIN_SET){
-			LED_Register_Bits ^= 00000100;
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) != GPIO_PIN_SET && (debounce & 0b00000100) != 0b00000000){
+			LED_Register_Bits ^= 0b00000100;
+			Button_Register_Bits |= 0b00000100;
+			debounce &= 0b11111011;
+		}else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) != GPIO_PIN_RESET){
+			debounce |= 0b00000100;
+			Button_Register_Bits &= 0b11111011;
 		}
 
 		vTaskDelay(100);
